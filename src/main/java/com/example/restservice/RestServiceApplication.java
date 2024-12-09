f17e715b9e458fb98705991d24493ca7178e01cd
KamilNnowakowski01 <kamil.nowakowski01@microsoft.wsei.edu.pl>
2024-12-09 17:43:41 +0100

    Update RestServiceApplication.java
    x

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -1,13 +1,33 @@
 package com.example.restservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class RestServiceApplication {
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
-	}
+    @GetMapping
+    public Greeting greeting() {
+        return new Greeting(1, "Hello, World!");
+    }
+}
 
+class Greeting {
+    private long id;
+    private String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
